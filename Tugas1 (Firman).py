c170c83f5728ed41fbe492f44b92dccd752b6bb2
firman Syah <56372587+firman660@users.noreply.github.com>
2019-10-26 13:33:49 +0700

    Add files via upload
    tugas1

---
+++ Tugas1 (Firman).py
@@ -0,0 +1,11 @@
+#INI Variabel & Nilai
+
+namalengkap = "Firman Syah"
+namapanggilan = "Firman"
+npm = "311910568"
+tempattgllahir = "Bekasi"
+umur = "17"
+nohp = "+62895-3747-89239"
+alamat = "Kp. Pulo kukun Rt 001/Rw 004 Desa Sukahurip Kec. Sukatani Kab. Bekasi"
+
+print("Assalamu'alaikum Wr. Wb, \n Let me introduce my self. My name is",namalengkap,", but you can call me",namapanggilan,".\n My NPM is",npm,".\n I was born in",tempattgllahir,",and i am",umur,"years old.\n I am very glad if you want to invite my house in",alamat,".\n So, don't forget to call me before with number",nohp,"\n Thank You")
